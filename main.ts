8503b39c9d1a5fa1b5db2eb9c67d8280f2693d06
Smaug546 <76572351+Smaug546@users.noreply.github.com>
2020-12-26 00:24:58 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,118 @@
+input.onGesture(Gesture.Shake, function () {
+    liczba_oczek = randint(1, 36)
+})
+let liczba_oczek = 0
+liczba_oczek = 0
+while (liczba_oczek == 0) {
+    basic.showString("potrzasnij")
+}
+basic.forever(function () {
+    if (liczba_oczek == 1) {
+        basic.showString("" + (1 + 1))
+    }
+    if (liczba_oczek == 2) {
+        basic.showString("" + (1 + 2))
+    }
+    if (liczba_oczek == 3) {
+        basic.showString("" + (1 + 3))
+    }
+    if (liczba_oczek == 4) {
+        basic.showString("" + (1 + 4))
+    }
+    if (liczba_oczek == 5) {
+        basic.showString("" + (1 + 5))
+    }
+    if (liczba_oczek == 6) {
+        basic.showString("" + (1 + 6))
+    }
+    if (liczba_oczek == 7) {
+        basic.showString("" + (2 + 1))
+    }
+    if (liczba_oczek == 8) {
+        basic.showString("" + (2 + 2))
+    }
+    if (liczba_oczek == 9) {
+        basic.showString("" + (2 + 3))
+    }
+    if (liczba_oczek == 10) {
+        basic.showString("" + (2 + 4))
+    }
+    if (liczba_oczek == 11) {
+        basic.showString("" + (2 + 5))
+    }
+    if (liczba_oczek == 12) {
+        basic.showString("" + (2 + 6))
+    }
+    if (liczba_oczek == 13) {
+        basic.showString("" + (3 + 1))
+    }
+    if (liczba_oczek == 14) {
+        basic.showString("" + (3 + 2))
+    }
+    if (liczba_oczek == 15) {
+        basic.showString("" + (3 + 3))
+    }
+    if (liczba_oczek == 16) {
+        basic.showString("" + (3 + 4))
+    }
+    if (liczba_oczek == 17) {
+        basic.showString("" + (3 + 5))
+    }
+    if (liczba_oczek == 18) {
+        basic.showString("" + (3 + 6))
+    }
+    if (liczba_oczek == 19) {
+        basic.showString("" + (4 + 1))
+    }
+    if (liczba_oczek == 20) {
+        basic.showString("" + (4 + 2))
+    }
+    if (liczba_oczek == 21) {
+        basic.showString("" + (4 + 3))
+    }
+    if (liczba_oczek == 22) {
+        basic.showString("" + (4 + 4))
+    }
+    if (liczba_oczek == 23) {
+        basic.showString("" + (4 + 5))
+    }
+    if (liczba_oczek == 24) {
+        basic.showString("" + (4 + 6))
+    }
+    if (liczba_oczek == 25) {
+        basic.showString("" + (5 + 1))
+    }
+    if (liczba_oczek == 26) {
+        basic.showString("" + (5 + 2))
+    }
+    if (liczba_oczek == 27) {
+        basic.showString("" + (5 + 3))
+    }
+    if (liczba_oczek == 28) {
+        basic.showString("" + (5 + 4))
+    }
+    if (liczba_oczek == 29) {
+        basic.showString("" + (5 + 5))
+    }
+    if (liczba_oczek == 30) {
+        basic.showString("" + (5 + 6))
+    }
+    if (liczba_oczek == 31) {
+        basic.showString("" + (6 + 1))
+    }
+    if (liczba_oczek == 32) {
+        basic.showString("" + (6 + 2))
+    }
+    if (liczba_oczek == 33) {
+        basic.showString("" + (6 + 3))
+    }
+    if (liczba_oczek == 34) {
+        basic.showString("" + (6 + 4))
+    }
+    if (liczba_oczek == 35) {
+        basic.showString("" + (6 + 5))
+    }
+    if (liczba_oczek == 36) {
+        basic.showString("" + (6 + 6))
+    }
+})
