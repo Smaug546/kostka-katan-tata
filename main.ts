8b564d4b9025ea0a1f24426113a660e28f183899
Smaug546 <76572351+Smaug546@users.noreply.github.com>
2020-12-26 17:46:39 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,87 @@ while (liczba_oczek == 0) {
     basic.showString("potrzasnij")
 }
 basic.forever(function () {
+    if (liczba_oczek >= 16 && liczba_oczek <= 21) {
+        basic.showLeds(`
+            . . # # #
+            . . . . #
+            . . . # .
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (liczba_oczek >= 11 && liczba_oczek <= 15) {
+        basic.showLeds(`
+            . . . # #
+            . . # . .
+            . . # # #
+            . . # . #
+            . . # # #
+            `)
+    }
+    if (liczba_oczek >= 26 && liczba_oczek <= 22) {
+        basic.showLeds(`
+            . . # # #
+            . . # . #
+            . . # # #
+            . . # . #
+            . . # # #
+            `)
+    }
+    if (liczba_oczek >= 7 && liczba_oczek <= 10) {
+        basic.showLeds(`
+            . . # # #
+            . . # . .
+            . . # # .
+            . . . . #
+            . . # # .
+            `)
+    }
+    if (liczba_oczek >= 27 && liczba_oczek <= 30) {
+        basic.showLeds(`
+            . . # # #
+            . . # . #
+            . . # # #
+            . . . . #
+            . . # # .
+            `)
+    }
+    if (liczba_oczek >= 4 && liczba_oczek <= 6) {
+        basic.showLeds(`
+            . . # . #
+            . . # . #
+            . . # # #
+            . . . . #
+            . . . . #
+            `)
+    }
+    if (liczba_oczek >= 31 && liczba_oczek <= 33) {
+        basic.showLeds(`
+            # . . # .
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
+            `)
+    }
+    if (liczba_oczek == 2 || liczba_oczek == 3) {
+        basic.showLeds(`
+            . . # # .
+            . . . . #
+            . . . # .
+            . . . . #
+            . . # # .
+            `)
+    }
+    if (liczba_oczek == 35 || liczba_oczek == 34) {
+        basic.showLeds(`
+            # . . # .
+            # . . # .
+            # . . # .
+            # . . # .
+            # . . # .
+            `)
+    }
     if (liczba_oczek == 1) {
         basic.showLeds(`
             . . # # .
@@ -16,312 +97,6 @@ basic.forever(function () {
             . . . # .
             . . # . .
             . . # # #
-            `)
-    }
-    if (liczba_oczek == 2) {
-        basic.showLeds(`
-            . . # # .
-            . . . . #
-            . . . # .
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 3) {
-        basic.showLeds(`
-            . . # . #
-            . . # . #
-            . . # # #
-            . . . . #
-            . . . . #
-            `)
-    }
-    if (liczba_oczek == 4) {
-        basic.showLeds(`
-            . . # # #
-            . . # . .
-            . . # # .
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 5) {
-        basic.showLeds(`
-            . . . # #
-            . . # . .
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 6) {
-        basic.showLeds(`
-            . . # # #
-            . . . . #
-            . . . # .
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (liczba_oczek == 7) {
-        basic.showLeds(`
-            . . # # .
-            . . . . #
-            . . . # .
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 8) {
-        basic.showLeds(`
-            . . # . #
-            . . # . #
-            . . # # #
-            . . . . #
-            . . . . #
-            `)
-    }
-    if (liczba_oczek == 9) {
-        basic.showLeds(`
-            . . # # #
-            . . # . .
-            . . # # .
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 10) {
-        basic.showLeds(`
-            . . . # #
-            . . # . .
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 11) {
-        basic.showLeds(`
-            . . # # #
-            . . . . #
-            . . . # .
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (liczba_oczek == 12) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 13) {
-        basic.showLeds(`
-            . . # . #
-            . . # . #
-            . . # # #
-            . . . . #
-            . . . . #
-            `)
-    }
-    if (liczba_oczek == 14) {
-        basic.showLeds(`
-            . . # # #
-            . . # . .
-            . . # # .
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 15) {
-        basic.showLeds(`
-            . . . # #
-            . . # . .
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 16) {
-        basic.showLeds(`
-            . . # # #
-            . . . . #
-            . . . # .
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (liczba_oczek == 17) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 18) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 19) {
-        basic.showLeds(`
-            . . # # #
-            . . # . .
-            . . # # .
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 20) {
-        basic.showLeds(`
-            . . . # #
-            . . # . .
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 21) {
-        basic.showLeds(`
-            . . # # #
-            . . . . #
-            . . . # .
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (liczba_oczek == 22) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 23) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 24) {
-        basic.showLeds(`
-            # . . # .
-            # . # . #
-            # . # . #
-            # . # . #
-            # . . # .
-            `)
-    }
-    if (liczba_oczek == 25) {
-        basic.showLeds(`
-            . . . # #
-            . . # . .
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 26) {
-        basic.showLeds(`
-            . . # # #
-            . . . . #
-            . . . # .
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (liczba_oczek == 27) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 28) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 29) {
-        basic.showLeds(`
-            # . . # .
-            # . # . #
-            # . # . #
-            # . # . #
-            # . . # .
-            `)
-    }
-    if (liczba_oczek == 30) {
-        basic.showLeds(`
-            # . . # .
-            # . . # .
-            # . . # .
-            # . . # .
-            # . . # .
-            `)
-    }
-    if (liczba_oczek == 31) {
-        basic.showLeds(`
-            . . # # #
-            . . . . #
-            . . . # .
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (liczba_oczek == 32) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . # . #
-            . . # # #
-            `)
-    }
-    if (liczba_oczek == 33) {
-        basic.showLeds(`
-            . . # # #
-            . . # . #
-            . . # # #
-            . . . . #
-            . . # # .
-            `)
-    }
-    if (liczba_oczek == 34) {
-        basic.showLeds(`
-            # . . # .
-            # . # . #
-            # . # . #
-            # . # . #
-            # . . # .
-            `)
-    }
-    if (liczba_oczek == 35) {
-        basic.showLeds(`
-            # . . # .
-            # . . # .
-            # . . # .
-            # . . # .
-            # . . # .
             `)
     }
     if (liczba_oczek == 36) {
